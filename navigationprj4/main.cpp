635db90e53a7bef0a5b8193cd4e9bff4ec0e168d
skylarshi123 <skylarshi123@gmail.com>
2024-03-08 15:47:18 -0800

    Initial Commit

---
+++ navigationprj4/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  navigationprj4
+//
+//  Created by Skylar Shi on 3/8/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
