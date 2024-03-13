dd80fe07e70339eefd174ebba831eb3b21345c0f
skylarshi123 <skylarshi123@gmail.com>
2024-03-13 15:40:13 -0700

    tour not working

--- navigationprj4/main.cpp
+++ navigationprj4/main.cpp
@@ -1,93 +1,94 @@
-//#include <cstdio>
-//#include <iomanip>
-//#include <iostream>
-//#include <map>
-//#include <vector>
-//#include "HashMap.h"
-//
-////#include "geodb.h"
-////#include "router.h"
-//#include "stops.h"
-//#include "tourcmd.h"
+#include <cstdio>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <vector>
+#include "HashMap.h"
+#include "tour_generator.h"
+#include "router.h"
 //#include "geodb.h"
-////#include "tour_generator.h"
-//
-//using namespace std;
+//#include "router.h"
+#include "stops.h"
+#include "tourcmd.h"
+#include "geodb.h"
+//#include "tour_generator.h"
 
-//void print_tour(vector<TourCommand>& tcs)
-//{
-//    double total_dist = 0;
-//    std::string direction;
-//    double street_distance = 0;
-//
-//    cout << "Starting tour...\n";
-//
-//    for (size_t i = 0; i < tcs.size(); ++i)
-//    {
-//        if (tcs[i].get_command_type() == TourCommand::commentary)
-//        {
-//            cout << "Welcome to " << tcs[i].get_poi() << "!\n";
-//            cout << tcs[i].get_commentary() << "\n";
-//        }
-//        else if (tcs[i].get_command_type() == TourCommand::turn)
-//        {
-//            cout << "Take a " << tcs[i].get_direction() << " turn on " << tcs[i].get_street() << endl;
-//        }
-//        else if (tcs[i].get_command_type() == TourCommand::proceed)
-//        {
-//            total_dist += tcs[i].get_distance();
-//            if (direction.empty())
-//                direction = tcs[i].get_direction();
-//            street_distance += tcs[i].get_distance();
-//            if (i+1 < tcs.size() && tcs[i+1].get_command_type() == TourCommand::proceed
-//                && tcs[i+1].get_street() == tcs[i].get_street() && tcs[i].get_street() != "a path")
-//            {
-//                continue;
-//            }
-//
-//            cout << "Proceed " << std::fixed << std::setprecision(3) << street_distance << " miles " << direction << " on " << tcs[i].get_street() << endl;
-//            street_distance = 0;
-//            direction.clear();
-//        }
-//    }
-//
-//    cout << "Your tour has finished!\n";
-//    cout << "Total tour distance: " << std::fixed << std::setprecision(3) << total_dist << " miles\n";
-//}
+using namespace std;
 
-//int main(int argc, char *argv[])
-//{
-//    if (argc != 3)
-//    {
-//        cout << "usage: BruinTour mapdata.txt stops.txt\n";
-//        return 1;
-//    }
-//
-//    GeoDatabase geodb;
-//    if (!geodb.load(argv[1]))
-//    {
-//        cout << "Unable to load map data: " << argv[1] << endl;
-//        return 1;
-//    }
-//
-//    Router router(geodb);
-//    TourGenerator tg(geodb, router);
-//
-//    Stops stops;
-//    if (!stops.load(argv[2]))
-//    {
-//        cout << "Unable to load tour data: " << argv[2] << endl;
-//        return 1;
-//    }
-//
-//    std::cout << "Routing...\n\n";
-//
-//    vector<TourCommand> tcs = tg.generate_tour(stops);
-//    if (tcs.empty())
-//        cout << "Unable to generate tour!\n";
-//    else
-//        print_tour(tcs);
-//}
+void print_tour(vector<TourCommand>& tcs)
+{
+    double total_dist = 0;
+    std::string direction;
+    double street_distance = 0;
+
+    cout << "Starting tour...\n";
+
+    for (size_t i = 0; i < tcs.size(); ++i)
+    {
+        if (tcs[i].get_command_type() == TourCommand::commentary)
+        {
+            cout << "Welcome to " << tcs[i].get_poi() << "!\n";
+            cout << tcs[i].get_commentary() << "\n";
+        }
+        else if (tcs[i].get_command_type() == TourCommand::turn)
+        {
+            cout << "Take a " << tcs[i].get_direction() << " turn on " << tcs[i].get_street() << endl;
+        }
+        else if (tcs[i].get_command_type() == TourCommand::proceed)
+        {
+            total_dist += tcs[i].get_distance();
+            if (direction.empty())
+                direction = tcs[i].get_direction();
+            street_distance += tcs[i].get_distance();
+            if (i+1 < tcs.size() && tcs[i+1].get_command_type() == TourCommand::proceed
+                && tcs[i+1].get_street() == tcs[i].get_street() && tcs[i].get_street() != "a path")
+            {
+                continue;
+            }
+
+            cout << "Proceed " << std::fixed << std::setprecision(3) << street_distance << " miles " << direction << " on " << tcs[i].get_street() << endl;
+            street_distance = 0;
+            direction.clear();
+        }
+    }
+
+    cout << "Your tour has finished!\n";
+    cout << "Total tour distance: " << std::fixed << std::setprecision(3) << total_dist << " miles\n";
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc != 3)
+    {
+        cout << "usage: BruinTour mapdata.txt stops.txt\n";
+        return 1;
+    }
+
+    GeoDatabase geodb;
+    if (!geodb.load(argv[1]))
+    {
+        cout << "Unable to load map data: " << argv[1] << endl;
+        return 1;
+    }
+
+    Router router(geodb);
+    TourGenerator tg(geodb, router);
+
+    Stops stops;
+    if (!stops.load(argv[2]))
+    {
+        cout << "Unable to load tour data: " << argv[2] << endl;
+        return 1;
+    }
+
+    std::cout << "Routing...\n\n";
+
+    vector<TourCommand> tcs = tg.generate_tour(stops);
+    if (tcs.empty())
+        cout << "Unable to generate tour!\n";
+    else
+        print_tour(tcs);
+}
 
 
 //int main() {
@@ -124,27 +125,27 @@
 //    return 0;
 //}
 
-#include "router.h"
-#include "geodb.h"
-#include "geopoint.h"
-
-#include <vector>
-#include <iostream>
-
-using namespace std;
-
-int main() {
-    GeoDatabase db;
-    db.load("/Users/skylarshi/Desktop/navigationprj4/navigationprj4/mapdata.txt");
-    Router router(db);
-    GeoPoint pt1;
-    GeoPoint pt2;
-    db.get_poi_location("Diddy Riese", pt1);
-    db.get_poi_location("Ami Sushi", pt2);
-    vector<GeoPoint> route = router.route(pt1, pt2);
-
-    for(const auto& point : route) {
-        cerr << point.to_string() << endl;
-    }
-
-}
+//#include "router.h"
+//#include "geodb.h"
+//#include "geopoint.h"
+//
+//#include <vector>
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main() {
+//    GeoDatabase db;
+//    db.load("/Users/skylarshi/Desktop/navigationprj4/navigationprj4/mapdata.txt");
+//    Router router(db);
+//    GeoPoint pt1;
+//    GeoPoint pt2;
+//    db.get_poi_location("Diddy Riese", pt1);
+//    db.get_poi_location("Ami Sushi", pt2);
+//    vector<GeoPoint> route = router.route(pt1, pt2);
+//
+//    for(const auto& point : route) {
+//        cerr << point.to_string() << endl;
+//    }
+//
+//}
--- navigationprj4/tour_generator.cpp
+++ navigationprj4/tour_generator.cpp
@@ -6,6 +6,12 @@
 //
 
 #include "tour_generator.h"
+#include "stops.h"
+#include "tourcmd.h"
+#include <vector>
+#include "geotools.h"
+
+using namespace std;
 
 TourGenerator::TourGenerator(const GeoDatabaseBase& geodb, const RouterBase& router){
     m_geodatabase = &geodb;
@@ -15,5 +21,65 @@ TourGenerator::TourGenerator(const GeoDatabaseBase& geodb, const RouterBase& rou
 TourGenerator::~TourGenerator(){}
 
 std::vector<TourCommand> TourGenerator::generate_tour(const Stops& stops) const{
-    return std::vector<TourCommand>();
+    vector<TourCommand> tour_commands;
+    int numStops = stops.size();
+    for(int i = 0; i < numStops; i++){
+        string stopName;
+        string stopDescription;
+        //returns empty vector if can't find, while also getting the info bc its passed in by reference
+        if(!stops.get_poi_data(i, stopName, stopDescription)) return {};
+        TourCommand commentaryCmd;
+        commentaryCmd.init_commentary(stopName, stopDescription);
+        tour_commands.push_back(commentaryCmd);
+        GeoPoint currentStopPoint;
+        if(i < numStops - 1){
+            if(!m_geodatabase->get_poi_location(stopName, currentStopPoint)) return {};
+        }
+        
+        string nextStopName;
+        string nextStopDescription;
+        if(!stops.get_poi_data(i+1, nextStopName, nextStopDescription)) return {};
+        GeoPoint nextStopPoint;
+        if(!m_geodatabase->get_poi_location(nextStopName, nextStopPoint)) return {};
+        vector<GeoPoint> route = m_routerbase->route(currentStopPoint, nextStopPoint);
+        if(route.empty()) return {};
+        
+        for(int j = 0; j < route.size() - 1; j++){
+            GeoPoint p1 = route[j];
+            GeoPoint p2 = route[j+1];
+            
+            string streetName = m_geodatabase->get_street_name(p1, p2);
+            double distance = distance_earth_miles(p1, p2);
+            string direction;
+            if(angle_of_line(p1,p2)<22.5) direction = "east";
+            else if(angle_of_line(p1,p2)<67.5) direction = "northeast";
+            else if(angle_of_line(p1,p2)<112.5) direction = "north";
+            else if(angle_of_line(p1,p2)<157.5) direction = "northwest";
+            else if(angle_of_line(p1,p2)<202.5) direction = "west";
+            else if(angle_of_line(p1,p2)<247.5) direction = "southwest";
+            else if(angle_of_line(p1,p2)<292.5) direction = "south";
+            else if(angle_of_line(p1,p2)<337.5) direction = "southeast";
+            else direction = "east";
+
+            TourCommand proceedCmd;
+            proceedCmd.init_proceed(direction, streetName, distance, p1, p2);
+            tour_commands.push_back(proceedCmd);
+            
+            if(j < route.size()-2){
+                GeoPoint p3 = route[j+2];
+                string streetNameNext = m_geodatabase->get_street_name(p2, p3);
+                double angle = angle_of_turn(p1, p2, p3);
+                
+                if(streetName != streetNameNext && angle != 0) {
+                    string turnDirection;
+                    if(angle >= 1 && angle < 180) turnDirection = "left";
+                    else turnDirection = "right";
+                    TourCommand turnCmd;
+                    turnCmd.init_turn(turnDirection, streetNameNext);
+                    tour_commands.push_back(turnCmd);
+                }
+            }
+        }
+    }
+    return tour_commands;
 }
